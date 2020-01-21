4ca4e9270c4991bca0878ab9a4773f6998bdc163
Long Nguyen <lpn@mit.edu>
2020-01-21 11:45:13 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
-sudo apt install git curl wget python python3 python-pip python3-pip
+sudo apt install git curl wget python python3 python-pip python3-pip net-tools
 
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sed -i '/plugins=(git)/d' ~/.zshrc
-echo "plugins=(git zsh-autosuggestions)" >> ${ZDOTDIR:-$HOME}/.zshrc
+sed 's/plugins=(git)/plugins=(git zsh-autosuggestions)/g' ~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting
 echo "source ${(q-)PWD}/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
 
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
+
+git clone https://github.com/preservim/nerdtree.git ~/.vim/pack/vendor/start/nerdtree
+vim -u NONE -c "helptags ~/.vim/pack/vendor/start/nerdtree/doc" -c q
